70882e1fa9063e29287a65b6e93e52c7e7dbd5e2
ErickAsencios271010 <erickalc2710@gmail.com>
2025-07-01 16:31:01 -0500

    Revert "Añadir HomePage.js y otros archivos"
    This reverts commit 324ebd4c5040480615ffab86c557ebdf99da5870.

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import "./Footer.css"; // crea este archivo si quieres estilos
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <small>© {new Date().getFullYear()} Grupo 3 - Software Inteligente</small>
-    </footer>
-  );
-}
